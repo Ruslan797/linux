522f1ee46ec0bc6c72b384c815a2c37a9230cfd5
Ruslan797 <36092163+Ruslan797@users.noreply.github.com>
2025-04-19 23:00:39 +0200

    Update permission_checker.sh

--- Home_work_11/permission_checker.sh
+++ Home_work_11/permission_checker.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+TARGET_DIR="/opt/210225-ptm"
+
+        find "$TARGET_DIR" -type f -name "*.sh" | while read FILE; do
+        if [ ! -x "$FILE" ]; then
+        chmod +x "$FILE"
+        echo "added permission to execute: $FILE"
+        else
+        echo "already executable: $FILE"
+        fi
+        done
+
+echo "Done"
+
+
 
